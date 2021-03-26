8d05fe411d29abe5e26bc5908a461231c108a55d
vageeshn <alokatulya@gmail.com>
2021-03-26 18:12:44 +0530

    test

--- js/Game.js
+++ js/Game.js
@@ -26,6 +26,12 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100,200);
+    car2 = createSprite(300,200);
+    car3 = createSprite(500,200);
+    car4 = createSprite(700,200);
+    
+    cars = [car1,car2,car3,car4];
   }
 
   play(){
@@ -35,16 +41,25 @@ class Game {
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      //var display_position = 130;
+      //index of the array
+      var index = 0;
+      //x and y position of the cars
+      var x = 0;
+      var y;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
-
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        //add 1 to the index for every loop
+        index = index+1;
+        //positioning the cars a little away from each other in x direction
+        x = x+200;
+        //use data from the database to display the cars in y direction
+        y = displayHeight-allPlayers[plr].distance;
+        cars[index-1].x = x;
+        cars[index-1].y = y;
+        if (index ===  player.index)
+            cars[index-1].shapeColor = "red";
+            camera.position.x = displayWidth/2;
+            camera.position.y = cars[index-1].y;
       }
     }
 
@@ -52,5 +67,6 @@ class Game {
       player.distance +=50
       player.update();
     }
+    drawSprites();
   }
 }
